77e9d02410fc585a36171bfa359dcb8387400053
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2024-07-28 11:49:09 +0800

    Update Paths.hx

--- source/Paths.hx
+++ source/Paths.hx
@@ -458,7 +458,7 @@ class Paths
 		}
 		#end
 		// I hate this so god damn much
-		var gottenPath:String = Sys.getCwd() + ('$path/$key.$SOUND_EXT', SOUND, library);
+		var gottenPath:String = Sys.getCwd() + getPath('$path/$key.$SOUND_EXT', SOUND, library);
 		gottenPath = gottenPath.substring(gottenPath.indexOf(':') + 1, gottenPath.length);
 		// trace(gottenPath);
 		if(!currentTrackedSounds.exists(gottenPath))
